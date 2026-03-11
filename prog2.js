c0c12868cf8ffb85510e38dd331e1cd7b5b7ccb2
ELIAS PEREIRA SANTOS <elias.santos@educacao.senac.rio>
2026-03-11 20:38:08 -0300

    arquivo prog2.js alterado

--- prog2.js
+++ prog2.js
@@ -2,6 +2,7 @@ let valor = 8;
 console.log("O conteúdo do valor é: " + valor);
 
 /*
+11/03
 Agora adicione este arquivo ao seu git
 Depois faça um commit sinalizando o que você fez
 Envie para o github
