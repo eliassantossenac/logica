41311e47fd645db5efebb28ca7e66952156c01f5
ELIAS PEREIRA SANTOS <elias.santos@educacao.senac.rio>
2026-03-11 20:35:37 -0300

    arquivo prog2.js criado

---
+++ prog2.js
@@ -0,0 +1,8 @@
+let valor = 8;
+console.log("O conteúdo do valor é: " + valor);
+
+/*
+Agora adicione este arquivo ao seu git
+Depois faça um commit sinalizando o que você fez
+Envie para o github
+*/
